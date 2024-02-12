2f9f1a53343bb1366c98983d0d5d0ab8b7fbea4a
kamyoo <kamy.alinejad@gmail.com>
2024-02-12 12:57:46 +0100

    add

--- main.js
+++ main.js
@@ -22,6 +22,12 @@ const pForm = document.getElementById("playerForm");
 
 pForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    let p1 = document.getElementById('playerOneInput');
-    console.log(p1.value)
+    const p1 = document.getElementById('playerOneInput');
+    const p2 = document.getElementById('playerTwoInput');
+    const p1FightStyle = document.querySelector("#fightingStyle1", "select").value;
+    const p2FightStyle = document.querySelector("#fightingStyle2", "select").value;
+    console.log(p1.value, "-", p1FightStyle, p2.value, "-", p2FightStyle);
+
+    const p1Wrp = document.getElementById('playerOneWrap');
+        p1Wrp.append(p1.value);
 });
