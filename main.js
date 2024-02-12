52647a671a502f9555fb7c0eeb5ad2de403becb9
kamyoo <kamy.alinejad@gmail.com>
2024-02-12 11:37:05 +0100

    ..

--- main.js
+++ main.js
@@ -3,9 +3,17 @@ import Fighters from "./modules/fighters.js";
 const fighter1 = new Fighters("Tobbe", "karate");
 fighter1.present();
 
-document.getElementById('startFightBtn').addEventListener('submit', fightForm());
-function fightForm(event) {
-    let playerOneValue = document.getElementById('playerOneInput').value;
+// function fightForm(event) {
+//     event.preventDefault();
+//     const p1Wrp = document.getElementById('playerOneWrap');
+//     let playerOneValue = document.getElementById('playerOneInput').value;
     
-    playerOneValue.
-}
+//     // p1Wrp.innerHTML = playerOneValue
+//     // document.body.p1Wrp.appendChild('h1');
+//     console.log(playerOneValue.value)
+// }
+
+// document.getElementById('startFightBtn').addEventListener('submit', (event) => {
+//     const p1 = document.getElementById('playerOneInput');
+//     console.log(p1.value)
+// });
