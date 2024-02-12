9de48a231d943feed4c95de558b86255c46db903
kamyoo <kamy.alinejad@gmail.com>
2024-02-12 11:01:33 +0100

    update

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ import Fighters from "./modules/fighters.js";
 
 const fighter1 = new Fighters("Tobbe", "karate");
 fighter1.present();
+
+document.getElementById('startFightBtn').addEventListener('submit', fightForm());
+function fightForm(event) {
+    let playerOneValue = document.getElementById('playerOneInput').value;
+    
+    playerOneValue.
+}
